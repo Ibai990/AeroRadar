06d0a3840d6108e691a071052b054d49e1fcf351
Ibai990 <ibai.olmosah@elorrieta-errekamari.com>
2026-09-24 11:59:39 +0200

    Limpiar plantilla y añadido el endpoint de status

---
+++ AeroRadar/Controllers/StatusController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AeroRadar.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StatusController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            service = "AeroRadar API",
+            status = "running",
+            serverTimeUtc = DateTime.UtcNow
+        });
+    }
+}
